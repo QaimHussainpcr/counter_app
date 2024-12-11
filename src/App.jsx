11698fc2129616752e5221fb82b094af42e9f2ef
Qaim Hussain <qaimpcr89@gmail.com>
2024-12-11 22:31:13 +0500

    Counter app

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react"
+import "./App.css"
+function App() {
+  const [counter, setCounter] = useState(0);
+  const added = () => {
+   
+    if(counter < 20){
+      setCounter(counter + 1);
+    }
+    
+  }
+  const remove = () => {
+    if(counter > 0){
+      setCounter(counter - 1);
+    }
+    
+  }
+
+  return (
+    <div className="container">
+        <div className="count">
+          <h1 className="heading">Counter App</h1>
+          <h2 className="main">Counter : {counter}</h2>
+          <button onClick={added}>Add Value</button>
+          <button onClick={remove}>Remove Value</button>
+        </div>      
+    </div>
+  )
+}
+
+export default App
